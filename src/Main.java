7ec8a312bed8308eedcb6000b4266d1508c6cebe
FangWolf <siyu2510@163.com>
2018-05-22 22:39:17 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = 0, x=0;
+        for(int i = 0; i < n; i++){
+            x = sc.nextInt();
+
+        }
+        System.out.println(x);
+    }
+}
